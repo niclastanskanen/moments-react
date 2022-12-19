d8d89f32327be4275bf153bdf812267d34482e46
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-19 09:55:47 +0000

    add dropdown to comments and handledelete comments

--- src/pages/posts/comments/Comment.js
+++ src/pages/posts/comments/Comment.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { axiosRes } from '../../../api/axiosDefaults';
 import Avatar from '../../../components/Avatar';
+import { MoreDropdown } from '../../../components/MoreDropdown';
+import { useCurrentUser } from '../../../contexts/CurrentUserContext';
 import styles from "../../../styles/Comment.module.css";
 
 const Comment = (props) => {
@@ -10,9 +13,31 @@ const Comment = (props) => {
         profile_image,
         owner,
         updated_at,
-        content
+        content,
+        id,
+        setPost,
+        setComments,
     } = props;
-    
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}`)
+            setPost(prevPost => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1
+                }]
+            }));
+            setComments(prevComments => ({
+                results: prevComments.results.filter(comment => comment.id !== id),
+            }));
+        } catch (err) {
+    }
+    };
+
   return (
     <div>
         <hr />
@@ -25,6 +50,9 @@ const Comment = (props) => {
                 <span className={styles.Date}>{updated_at}</span>
                 <p>{content}</p>
             </Media.Body>
+            {is_owner && (
+                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+            )}
         </Media>
     </div>
   )
