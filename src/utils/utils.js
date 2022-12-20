502b1d82bb111966e90053bc5a4d388b53e50e1f
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-20 08:42:26 +0000

    add follow functions for profiles

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,3 +14,24 @@ export const fetchMoreData = async (resource, setResource) => {
     }));
   } catch (err) {}
 };
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+  return profile.id === clickedProfile.id
+  ? // this is the profile i clicked on,
+    // update its followers count and set its following id
+    {
+      ...profile,
+      followers_count: profile.followers_count + 1,
+      following_id,
+    }
+  : profile.is_owner
+  ? // this is the profile of the logged in user
+    // update its following count
+    {
+      ...profile,
+      following_count: profile.following_count + 1
+    }
+  : // this is not the profile the user clicked on or the profile
+    // the user owns, so just return it unchanged
+    profile;
+}
